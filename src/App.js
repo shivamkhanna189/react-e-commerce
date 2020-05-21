dd8a265c5cb072f257555ae5288356d67f7a74f0
ShivamKhanna <shivamkhanna1995@gmail.com>
2020-05-21 09:51:07 +0530

    Header component added.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import {Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import { ShopsPage } from './pages/shops/shops.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Route exact path="/" component={ HomePage }/>
       <Route path="/shops" component = {ShopsPage}/>
     </div>
--- src/components/preview-collection/preview.collection.jsx
+++ src/components/preview-collection/preview.collection.jsx
@@ -4,7 +4,7 @@ import CollectionItem from "../../components/collection-item/collection-item.com
 
 const CollectionPreview = ({title, items})=>{
     return (<div  className="collection-preview">
-                <h1 className="title">{title} </h1>
+                <h1 className="title">{title.toUpperCase()} </h1>
                 <div className="preview">
                     {items.filter((item,index)=>index<4).map(({id,...reaminingItemData})=>(<CollectionItem key={id} {...reaminingItemData}/>))}
                  </div>
